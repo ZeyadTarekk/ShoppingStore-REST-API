4c0195ea12ed5725865c3704be5d203cd1225b7e
Zeyad Tarek <zeyad.ta01@gmail.com>
2022-09-16 23:28:52 +0200

    create a user and save the token beforeall

--- src/models/tests/product_spec.ts
+++ src/models/tests/product_spec.ts
@@ -1,8 +1,26 @@
 import { Product, ProductStore } from "../product";
-
+import { User, UserStore } from "../user";
+const supertest = require("supertest");
+import app from "../../server";
+import Client from "../../database";
 const store = new ProductStore();
+const request = supertest(app);
+
+let token;
 
 describe("Book Model", () => {
+  beforeAll(async () => {
+    const testUser: User = {
+      id: 0,
+      first_name: "zeyad",
+      last_name: "tarek",
+      password: "1234",
+    };
+
+    const requestData = JSON.stringify(testUser);
+    const response = await request.post("/users", requestData);
+    token = response.body;
+  });
   it("should have an index method", () => {
     expect(store.index).toBeDefined();
   });
@@ -11,17 +29,9 @@ describe("Book Model", () => {
     expect(store.show).toBeDefined();
   });
 
-  // it("should have a create method", () => {
-  //   expect(store.index).toBeDefined();
-  // });
-
-  // it("should have a update method", () => {
-  //   expect(store.index).toBeDefined();
-  // });
-
-  // it("should have a delete method", () => {
-  //   expect(store.index).toBeDefined();
-  // });
+  it("should have a create method", () => {
+    expect(store.create).toBeDefined();
+  });
 
   // it("create method should add a book", async () => {
   //   const result = await store.create({
