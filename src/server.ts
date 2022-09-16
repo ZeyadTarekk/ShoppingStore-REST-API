f3e02fb215ad543df004227ee7353e8f25e24d64
Zeyad Tarek <zeyad.ta01@gmail.com>
2022-09-16 18:37:07 +0200

    adding cors to the server

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import * as express from "express";
+const cors = require("cors");
 const bodyParser = require("body-parser");
 const app = express();
 const address: string = "0.0.0.0:3000";
 
 app.use(bodyParser.json());
+app.use(cors());
 
 app.get("/", function (req: express.Request, res: express.Response) {
   res.send("Hello World!");
