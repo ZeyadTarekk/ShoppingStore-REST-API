fb7a5e192a003375e557bd48a0406c1a7778b319
Zeyad Tarek <zeyad.ta01@gmail.com>
2022-09-16 18:53:29 +0200

    add show product handler

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -9,8 +9,14 @@ const index = async (req: express.Request, res: express.Response) => {
   res.json(products);
 };
 
+const show = async (req: express.Request, res: express.Response) => {
+  const product = await store.show(req.params.id);
+  res.json(product);
+};
+
 const product_routes = (app: express.Application) => {
   app.get("/products", index);
+  app.get("/products/:id", show);
 };
 
 export default product_routes;
